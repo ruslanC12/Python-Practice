3bdb28ebf3b3d3faf7718c30dcb0e156eb2f62b4
Ruslan Cahnovsky <ruslanc1414@gmail.com>
2020-02-13 14:16:29 -0600

    text

---
+++ Python-Practice/Sudoku/main.py
@@ -0,0 +1,10 @@
+impot numpie
+
+def checkIfPossible(board, number):
+    numpie
+
+def solver(board, index):
+    i = index
+    for x in 9:
+        if checkIfPossible(i, x):
+            solver(board, i++)
